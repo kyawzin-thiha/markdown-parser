63414719cdcbda445eed08976211ffdafebea99c
Kyaw Zin Thiha <github@kyawzinthiha.dev>
2024-01-19 17:10:56 +0000

    add favicon and change styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,52 @@
 'use client';
 
-import {useCallback, useState} from 'react';
-import {parseMarkdown} from '@/lib/parser';
+import { useCallback, useState } from 'react';
+import { parseMarkdown } from '@/lib/parser';
 import Editor from '@monaco-editor/react';
 import styles from './page.module.scss';
 
 export default function Home() {
-    const [parsedValue, setParsedValue] = useState<string>('');
+	const [parsedValue, setParsedValue] = useState<string>('// some comments');
+	const onChange = useCallback((value: string | undefined) => {
+		if (value) setParsedValue(parseMarkdown(value));
+	}, []);
 
-    const onChange = useCallback((value: string | undefined) => {
-        setParsedValue(parseMarkdown(value as string));
-    }, []);
 
-    return (
-        <main className={styles.main}>
-            <div className={styles.editorContainer}>
-                <Editor
-                    width="100%"
-                    height="100%"
-                    defaultLanguage="markdown"
-                    defaultValue="// some comment"
-                    className={styles.editor}
-                    onChange={onChange}
-                    options={{
-                        fontSize: 16,
-                        padding: {top: 10},
-                        trimAutoWhitespace: true,
-                        autoClosingQuotes: 'always',
-                        autoClosingBrackets: 'always',
-                        autoClosingOvertype: 'always',
-                        autoIndent: 'full',
-                        autoClosingComments: 'always',
-                        scrollbar: {vertical: 'hidden', horizontal: 'hidden'},
-                        cursorStyle: 'line',
-                        lineNumbers: 'off',
-                        minimap: {autohide: true}
-                    }}
-                />
-            </div>
-            <div className={styles.parserContainer}>
-                <div className={styles.parser} dangerouslySetInnerHTML={{__html: parsedValue}}/>
-            </div>
-        </main>
-    );
+	return (
+		<main className={styles.main}>
+			<div className={styles.header}>
+				<h1 className={styles.title}>Markdown Editor</h1>
+				<p className={styles.subtitle}>A powerful Markdown editor and parser built using Next.js</p>
+			</div>
+			<div className={styles.inputContainer}>
+				<div className={styles.editorContainer}>
+					<Editor
+						width="100%"
+						height="100%"
+						defaultLanguage="markdown"
+						defaultValue="// some comment"
+						className={styles.editor}
+						onChange={onChange}
+						options={{
+							fontSize: 16,
+							automaticLayout: true,
+							trimAutoWhitespace: true,
+							autoClosingQuotes: 'always',
+							autoClosingBrackets: 'always',
+							autoClosingOvertype: 'always',
+							autoIndent: 'full',
+							autoClosingComments: 'always',
+							scrollbar: { vertical: 'hidden', horizontal: 'hidden' },
+							cursorStyle: 'line',
+							lineNumbers: 'off',
+							minimap: { autohide: true },
+						}}
+					/>
+				</div>
+				<div className={styles.parserContainer}>
+					<div className={styles.parser} dangerouslySetInnerHTML={{ __html: parsedValue }} />
+				</div>
+			</div>
+		</main>
+	);
 }
